8708d35bfc2f3be527feb541e0d53764e33a206f
Renjie Deng <dengrenjie31@gmail.com>
2021-11-29 22:32:24 +0800

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 tar xvf build.tar.xz
-echo $PASSWORD | sudo -S rm -rf /var/www/rank
+sudo rm -rf /var/www/rank
 sudo mv build /var/www/rank
 sudo chown -R www-data:www-data /var/www/rank
